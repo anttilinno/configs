10ab5f8ad35383641d882ad10f9f6e9fa61b7326
Antti Linno <antti.linno@gmail.com>
2024-04-21 19:06:22 +0000

    Add nvim config files

---
+++ config/nvim/lua/plugins/init.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  require'lspconfig'.pyright.setup{}
+}
