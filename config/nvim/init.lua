579e8c22fbd1c43475ae57adf107461ca8ce5afa
Antti Linno <antti.linno@gmail.com>
2024-04-24 10:22:31 +0000

    Add lspsaga

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,6 +24,17 @@ require("lazy").setup({
       require "options"
     end,
   },
+  {
+    'nvimdev/lspsaga.nvim',
+    event = "LspAttach",
+    config = function()
+        require('lspsaga').setup({})
+    end,
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter', -- optional
+        'nvim-tree/nvim-web-devicons',     -- optional
+    }
+  },
 
   { import = "plugins" },
 }, lazy_config)
