9ffefa4d178592e183f2a79ad865d544e7721f19
Antti Linno <antti.linno@gmail.com>
2024-05-08 10:20:32 +0000

    Add some keybindings

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -7,4 +7,14 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Disable arrow keys
+map({ "n", "i", "v" }, "<Up>", "<Nop>")
+map({ "n", "i", "v" }, "<Left>", "<Nop>")
+map({ "n", "i", "v" }, "<Right>", "<Nop>")
+map({ "n", "i", "v" }, "<Down>", "<Nop>")
+
+-- Save with Ctrl + S
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Clear search hightlight
+map("n", "<leader><space>", "<cmd> noh <cr>")
